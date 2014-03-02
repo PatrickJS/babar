645a2f4bc84f5dcfcf196e42589e4a9993a4c94b
gdi2290 <github@gdi2290.com>
2014-03-01 16:16:29 -0800

    init database

---
+++ database/index.js
@@ -0,0 +1,18 @@
+'use strict';
+var _ = require('underscore');
+
+module.exports = function(app, config) {
+
+  var DB = {
+    get: function(data) {
+    },
+    sessions: function() {
+    },
+    questions: function() {
+    }
+  };
+
+  console.log('Database Initialized');
+
+  return DB;
+};
