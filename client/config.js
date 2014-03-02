a1ba7ec4bb5a2e8f2360820edbb00027fe94647a
gdi2290 <github@gdi2290.com>
2014-03-01 16:15:06 -0800

    add client/config

---
+++ client/config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('app')
+.config(function(cfpLoadingBarProvider) {
+  cfpLoadingBarProvider.includeSpinner = false;
+  cfpLoadingBarProvider.parentSelector = 'body';
+});
