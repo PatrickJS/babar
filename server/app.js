41515bc7960f6c1a8ffa792e1b4b36894389c84a
gdi2290 <github@gdi2290.com>
2014-03-01 16:17:37 -0800

    init server/app

---
+++ server/app.js
@@ -0,0 +1,54 @@
+var NODE_ENV = process.env.NODE_ENV = (process.env.NODE_ENV || 'production');
+var PORT = process.env.PORT = (process.env.PORT || 3000);
+var _ = require('underscore');
+var express = require('express');
+var stylus = require('stylus');
+var path = require('path');
+var cors = require('cors');
+var config = require('./config.json')[NODE_ENV];
+var mongodb = require('mongodb');
+
+module.exports = function(SERVER_ROOT) {
+  var app = express();
+  app.directory = SERVER_ROOT;
+  var database = require('../database')(app, config);
+
+  // Store all environment variables
+  app.set('port', PORT);
+  app.set('env', NODE_ENV);
+
+  app.configure(function() {
+    // App middleware
+    app.use(cors());
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(app.directory, 'client'));
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({secret: 'yolo'}));
+    // app.use(express.session());
+    app.use('/bower_components', express.static(path.join(app.directory, 'bower_components')));
+    app.use('/templates', express.static(path.join(app.directory, 'client/templates')));
+    app.use(express.static(path.join(app.directory, 'public')));
+    app.use(app.router);
+    app.use(stylus.middleware({
+      src: app.directory + '/client/styles',
+      dest: app.directory + '/public/stylesheets',
+      compile: function(str, path, fn) {
+        stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .render(fn);
+      }
+    }));
+  });
+
+
+  app.get('/*', function(req, res) {
+    return res.render('index', {env: app.get('env')});
+  });
+
+  return app;
+};
