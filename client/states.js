6fe7c6455b4eb904216c2f8b2fa7992cb241239e
gdi2290 <github@gdi2290.com>
2014-03-02 23:54:08 -0800

    refactor dashboard state

--- client/states.js
+++ client/states.js
@@ -6,26 +6,26 @@ angular.module('app')
   $stateProvider
   .state( 'app', {
     url: '',
-    template: '<div ui-view></div>',
+    templateUrl: '/templates/ng-view.html',
     abstract: true,
     controller: 'AppCtrl'
   })
-  // .state('app.dashboard', {
-  //   url: '/dashboard',
-  //   templateUrl: '/dashboard/template.html'
-  // });
+  .state('app.dashboard', {
+    url: '/dashboard',
+    templateUrl: '/templates/dashboard.html'
+  });
 
 })
 .config(function($locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
 })
 
-.controller('AppCtrl', function($scope) {
+.controller('AppCtrl', function($scope, $stateParams, $state) {
+  $scope.$state = $state;
+  $scope.$stateParams = $stateParams;
 
 })
 
-.run(function($rootScope, $stateParams, $state) {
-  $rootScope.$state = $state;
-  $rootScope.$stateParams = $stateParams;
+.run(function() {
 
 });
